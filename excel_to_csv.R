61501cd9b47da56390aa8c169a35353b9dd07eb4
jesseabrandt <jesse.a.brandt@gmail.com>
2025-07-09 01:32:24 -0400

    use package managaer

--- excel_to_csv.R
+++ excel_to_csv.R
@@ -1,7 +1,7 @@
-# Rewrite this in Python if possible
-library(readxl)
-library(tidyverse)
-
+if (!require(pacman)) {install.packages("pacman"); require(pacman)}
+#library(readxl)
+#library(tidyverse, warn.conflicts = FALSE)
+pacman::p_load(readxl, tidyverse)
 
 files <- Sys.glob("Patient X data files/*.xlsx") |>
   grep(pattern = "~", invert = T, value = T)
@@ -33,13 +33,17 @@ breathing_rate <- map(files, \(x)read_excel(x, sheet = "Breathing Rate")) %>%
   bind_rows() %>%
   #mutate(across(-Date, parse_number)) %>%
   group_by(Date) %>% #handle duplicates
-  summarize(Min = min(Min), Max = max(Max), Avg = mean(Avg), Median = median(Median)) %>%
+  summarize(
+    Min = min(Min), Max = max(Max), 
+    Avg = mean(Avg), Median = median(Median)) %>%
   rename_with(~paste0(.x, " Breathing Rate"), -Date) 
 
 numerical_data <- breathing_rate %>%
   full_join(heart_rate) %>%
   full_join(oxygen_level) %>%
   full_join(sleep)
+
+write_csv(numerical_data, "numerical_data.csv")
 
 #Text Data
 #sleep activities should go in table with observations and activities
